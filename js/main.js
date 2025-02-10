0239554ac42ab31cd1a407f99b90c443c7be4549
xu (Tom) weikai <xu000306@algonquinlive.com>
2025-02-10 12:40:16 -0500

    manage the log in different modes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
 //https://serverless-vercel-rouge.vercel.app/
 document.addEventListener("DOMContentLoaded", init);
-function init() {}
+
+const MODE = Object.freeze({
+  DEV: "DEV",
+  PROD: "PRODUCTION",
+  STAGING: "STAGING",
+});
+let mode = (() => {
+  //127.0.0.1:5500- origin
+  //127.0.0.1-hostname
+  if (location.hostname == "localhost" || location.hostname == "127.0.0.1") {
+    return MODE.DEV; //DEV
+  }
+  if (location.hostname.endsWith("github.io")) {
+    return MODE.STAGING;
+  }
+  if (location.hostname.includes("vercel.app")) {
+    return MODE.PROD;
+  }
+})();
+const log = (msg, level = "info") => {
+  switch (level) {
+    case "info":
+      if (mode === MODE.DEV) {
+        console.log(msg);
+      }
+      break;
+    case "warn":
+      console.warn(msg);
+
+      break;
+    case "error":
+      console.error(msg);
+  }
+};
+
+function init() {
+  //page is ready to use
+  log("hello");
+  log("yikes", "warn");
+  log("ah crap!!!", "error");
+}
